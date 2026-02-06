f0e930f8116533ca79ce45d8c56cc8be7f2765d7
Sahil Rawat <sahilrawatjj@gmail.com>
2026-02-06 23:40:56 +0530

    second Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar'
+import MainSection from './components/MainSection'
+import ServicesDiagram from './components/ServicesDiagram'
+import Footer from './components/Footer'
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <MainSection />
+            <ServicesDiagram />
+            <Footer />
+        </>
+    )
+}
+
+export default App
